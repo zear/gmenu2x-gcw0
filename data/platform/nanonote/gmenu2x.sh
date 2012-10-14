c302218f008343448234761fd79cba19164fed79
kyak <bas@bmail.ru>
2012-10-14 12:07:05 +0400

    update startup script for nanonote
    Change default font to 6x10 and add some explanations

--- data/platform/nanonote/gmenu2x.sh
+++ data/platform/nanonote/gmenu2x.sh
@@ -2,10 +2,23 @@
 
 source /etc/profile
 
+#Use Nanonote-specific 6x10 font. This font is very clear, but can't display colors
+#This results in 53x24 terminal size
 #setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
+# This is to display line drawing glyphs correctly (the font itself doesn't contain this information)
 #loadunimap /usr/share/setfont2/ben_uni.trans
 
-setfont /usr/share/kbd/consolefonts/kernel-6x11-font
+#Or use standard 6x11 font (supporting colors)
+#This results in 53x21 terminal size
+#setfont /usr/share/kbd/consolefonts/kernel-6x11-font
+
+#Some people prefer terminus font (supports colors and more glyphs)
+#This results in 53x20 terminal size
+#setfont /usr/share/kbd/consolefonts/ter-v12n.psf
+
+#We use 6x10 font by default (forked from standard 6x11)
+#This results in 53x24 terminal size
+setfont /usr/share/kbd/consolefonts/kernel-6x10-font
 
 trap "" hup
 

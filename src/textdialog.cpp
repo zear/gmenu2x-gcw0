zear/gmenu2x-gcw0

commit 59a456690b3b980ef6aa9365ab78fcc47e0d42dc

Fix build on 64-bit systems

src/textdialog.cpp
@@ -139,7 +139,7 @@ void TextDialog::exec() {
 				if (firstRow + rowsPerPage*2 -1 < text->size()) {
 					firstRow += rowsPerPage-1;
 				} else {
-					firstRow = max(0u, text->size() - rowsPerPage);
+					firstRow = max(0lu, text->size() - rowsPerPage);
 				}
 				break;
 			case InputManager::SETTINGS:

src/textmanualdialog.cpp
@@ -133,7 +133,7 @@ void TextManualDialog::exec() {
 				break;
 			case InputManager::ALTRIGHT:
 				if (firstRow + rowsPerPage*2 -1 < pages[page].text.size()) firstRow += rowsPerPage-1;
-				else firstRow = max(0u, pages[page].text.size() - rowsPerPage);
+				else firstRow = max(0lu, pages[page].text.size() - rowsPerPage);
 				break;
 			case InputManager::CANCEL:
 			case InputManager::SETTINGS:

commit abc461bf955184de75af31e750c9fe005c8f4872

Fix build on 32-bit targets...
This fixes commit 59a456690b3b980ef6aa9365ab78fcc47e0d42dc
which introduced a regression.

src/textdialog.cpp
@@ -139,7 +139,7 @@ void TextDialog::exec() {
 				if (firstRow + rowsPerPage*2 -1 < text->size()) {
 					firstRow += rowsPerPage-1;
 				} else {
-					firstRow = max(0lu, text->size() - rowsPerPage);
+					firstRow = max(0ul, (unsigned long) (text->size() - rowsPerPage));
 				}
 				break;
 			case InputManager::SETTINGS:

src/textmanualdialog.cpp
@@ -133,7 +133,7 @@ void TextManualDialog::exec() {
 				break;
 			case InputManager::ALTRIGHT:
 				if (firstRow + rowsPerPage*2 -1 < pages[page].text.size()) firstRow += rowsPerPage-1;
-				else firstRow = max(0lu, pages[page].text.size() - rowsPerPage);
+				else firstRow = max(0ul, (unsigned long) (pages[page].text.size() - rowsPerPage));
 				break;
 			case InputManager::CANCEL:
 			case InputManager::SETTINGS: